f63c7d8a781526e2f0710c649b52788046c47c86
Y.V <5196666qwe@gmail.com>
2019-07-11 14:02:05 +0800

    refactor(filter): add typeorm error filter

--- app/common/HttpExceptionFilter.ts
+++ app/common/HttpExceptionFilter.ts
@@ -1,24 +1,53 @@
 import { HttpException, ExceptionFilter, Catch, HttpStatus } from 'egg-pig';
+import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
+import { QueryFailedError } from 'typeorm/error/QueryFailedError';
 
-@Catch(HttpException)
+@Catch(EntityNotFoundError, QueryFailedError, HttpException)
 export class HttpExceptionFilter extends ExceptionFilter {
-    catch(exception: HttpException) {
+
+    catch(exception: HttpException | QueryFailedError | EntityNotFoundError) {
+
         const { ctx } = this;
 
-        const res = exception.getResponse();
+        let statusCode = HttpStatus.OK;
 
-        const { statusCode: code, error, message: msg } = res as any;
+        if (exception instanceof HttpException) {
 
-        if (code && code === HttpStatus.BAD_REQUEST) {
-            ctx.logger.warn(`[bad_request] ${JSON.stringify(res)}`)
-        } else if (/40\d/.test(code)) {
-            ctx.logger.warn(`[unauthorized] ${JSON.stringify(res)}`)
+            const res = exception.getResponse();
+
+            const { statusCode: code, error, message: msg } = res as any;
+
+            if (code && code === HttpStatus.BAD_REQUEST) {
+                ctx.logger.warn(`[bad_request] ${JSON.stringify(res)}`)
+            } else if (/40\d/.test(code)) {
+                ctx.logger.warn(`[unauthorized] ${JSON.stringify(res)}`)
+            } else {
+                ctx.logger.error(`[unhandled exception] ${JSON.stringify(res)}`)
+            }
+
+            ctx.status = code;
+            ctx.body = { code, error, msg }
+
+        } else if (exception instanceof EntityNotFoundError) {
+
+            statusCode = HttpStatus.NOT_FOUND;
+
         } else {
-            ctx.logger.error(`[unhandled exception] ${JSON.stringify(res)}`)
+
+            statusCode = HttpStatus.BAD_REQUEST;
+
         }
 
-        ctx.status = code;
-        ctx.body = { code, error, msg }
+        if (statusCode !== HttpStatus.OK) {
+
+            ctx.logger.error(`[unhandled exception] ${JSON.stringify(exception)}`)
+
+            ctx.status = statusCode;
+
+            ctx.body = { code: statusCode, error: exception.message, }
+        }
+
+
 
     }
 }
